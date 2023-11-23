81911d526fee83498639158469b465e37b2850ce
joshua chivile <joshuachivile@gmail.com>
2023-11-23 18:04:07 +0300

    Looping in Python

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
+import ipdb
+
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i <= 10:
+        print(i)
+        if(i == 1):
+            print("Happy New Year!")
+            break
+        else:
+            i-=1
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    new_list = list()
+    for i in int_list:
+        indiv_item = i**2
+        new_list.append(indiv_item)
+    return new_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    i = 0
+    while i < 100:
+        i+=1
+        if(i%3 == 0 and i%5 == 0):
+            print("FizzBuzz")
+        elif(i%3 == 0):
+            print("Fizz")
+        elif(i%5 == 0):
+            print("Buzz")
+        else:
+            print(i)
